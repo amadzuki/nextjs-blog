72a610dea613e96ef64884956340ee53bb2d5152
Ahmad Marzuki <ahmadmarzuki7777@gmail.com>
2020-06-02 20:06:23 +0700

    Implement parse date with dayjs

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,16 +1,25 @@
+import Head from "next/head"
+
+import dayjs from "dayjs"
+// import customParseFormat from "dayjs/plugin/customParseFormat"
+// dayjs.extend(customParseFormat)
+
 import Layout from "../../components/Layout"
 import { getAllPostIds, getPostData } from "../../lib/posts"
 
 const Post = ({ postData }) => {
+  const postDate = dayjs(postData.date).format("MMMM D, YYYY")
   return (
     <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
       <div style={{ maxWidth: "70%", margin: "auto" }}>
-        {" "}
         {postData.title}
         <br />
         {postData.id}
         <br />
-        {postData.date}
+        {postDate}
         <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }}></div>
       </div>
     </Layout>
